e265fd1f3fd0825d4303552ac22b9a4500ea00b5
Md. Ariful Islam <arifislam11ctg@gmail.com>
2026-05-23 00:38:23 -0400

    complain api done2

--- server.js
+++ server.js
@@ -10,8 +10,13 @@ const connectDatabase = require('./config/database')
 connectDatabase()
 
 // Server
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+module.exports = app
